36c8ccf17d6c0d509fb7716db85b6b8f4e2d4902
Daniele <rossi.daniele1999@gmail.com>
2020-11-10 16:06:01 +0100

    :pencil2: Update user model

--- server/api/v1/models/User.js
+++ server/api/v1/models/User.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
-  name: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
   firstname: { type: String, required: true },
