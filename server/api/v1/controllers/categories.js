7729e000584c2b57d227e9b503af579e1c5eb4e1
Filippo Momesso <filippomomesso@gmail.com>
2020-11-10 22:37:28 +0100

    :sparkles: Add category createOne and deleteOne logic

--- server/api/v1/controllers/categories.js
+++ server/api/v1/controllers/categories.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose');
 const Category = require('../models/Category');
 
-exports.getList = (req, res, next) => {
-
-};
+exports.getList = (req, res, next) => {};
 
 exports.getOne = (req, res, next) => {
   const id = req.params.categoryId;
@@ -14,9 +12,67 @@ exports.updateOne = (req, res, next) => {
 };
 
 exports.createOne = (req, res, next) => {
+  const category = {
+    name: req.body.name ? req.body.name : '',
+    icon: req.body.icon ? req.body.icon : '',
+  };
 
+  if (category.name === '' || category.icon === '') {
+    return res.status(400).json({
+      message: 'Empty data',
+    });
+  }
+
+  // TODO: Check if category icon is valid
+
+  Category.find({ name: category.name })
+    .exec()
+    .then((response) => {
+      if (response.length >= 1) {
+        return res.status(409).json({
+          message: 'Category already exists',
+        });
+      }
+      const categoryModel = new Category(category);
+      categoryModel.save((error) => {
+        if (error) {
+          console.log(error);
+          return res.status(500).json({
+            error: 'Error in saving new category',
+          });
+        }
+        return res.status(201).json({
+          message: 'Category created successfully',
+        });
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
 };
 
 exports.deleteOne = (req, res, next) => {
-  const id = req.params.categoryId;
+  const categoryId = req.params.categoryId ? req.params.categoryId : '';
+
+  if (categoryId === '') {
+    return res.status(400).json({
+      message: 'No category provided',
+    });
+  }
+
+  Category.findByIdAndDelete(categoryId)
+    .exec()
+    .then((result) => {
+      res.status(200).json({
+        message: `Category ${result.name} deleted`,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: 'Category deletion failed',
+      });
+    });
 };
