c808ea0306816f2de260831e0d5ac80b24f3b8b6
Filippo Momesso <filippomomesso@gmail.com>
2020-11-10 15:55:51 +0100

    :sparkles: Add login controller logic

--- server/api/v1/controllers/users.js
+++ server/api/v1/controllers/users.js
@@ -1,14 +1,53 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
 const User = require('../models/User');
 
 exports.login = (req, res, next) => {
-
+  User.find({ email: req.body.email })
+    .exec()
+    .then((user) => {
+      if (user.length < 1) {
+        return res.status(401).json({
+          message: 'Authentication failed',
+        });
+      }
+      bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+        if (err) {
+          return res.status(401).json({
+            message: 'Authentication failed',
+          });
+        }
+        if (result) {
+          const token = jwt.sign(
+            {
+              email: user[0].email,
+              userId: user[0]._id,
+            },
+            process.env.JWT_KEY,
+            {
+              expiresIn: '1h',
+            },
+          );
+          return res.status(200).json({
+            message: 'Authentication successful',
+            token,
+          });
+        }
+        return res.status(401).json({
+          message: 'Authentication failed',
+        });
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
 };
 
-exports.register = (req, res, next) => {
+exports.register = (req, res, next) => {};
 
-};
-
-exports.logout = (req, res, next) => {
-
-};
+exports.logout = (req, res, next) => {};
