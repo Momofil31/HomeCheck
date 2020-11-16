0663ec0057b0c1581c1c1b8cabf2511b409b3a76
Filippo Momesso <filippomomesso@MBP-di-Filippo.lan>
2020-11-16 14:13:27 +0100

    :bug: Fix missing key linting error

--- client/homecheck-vue/src/components/navigation/Navigation.vue
+++ client/homecheck-vue/src/components/navigation/Navigation.vue
@@ -1,8 +1,5 @@
 <template>
-  <v-navigation-drawer app
-        v-model="drawer"
-        :mini-variant.sync="mini"
-        permanent >
+  <v-navigation-drawer app v-model="drawer" :mini-variant.sync="mini" permanent>
     <v-list-item class="px-4">
       <v-list-item-icon>
         <v-icon>mdi-check</v-icon>
@@ -10,10 +7,7 @@
 
       <v-list-item-title>HomeCheck</v-list-item-title>
 
-      <v-btn
-        icon
-        @click.stop="mini = !mini"
-      >
+      <v-btn icon @click.stop="mini = !mini">
         <v-icon>mdi-chevron-left</v-icon>
       </v-btn>
     </v-list-item>
@@ -21,33 +15,34 @@
     <v-list dense>
       <NavigationItem
         v-for="item in items"
+        :key="item.title"
         :action="item.action"
         :icon="item.icon"
-        :title="item.title" />
+        :title="item.title"
+      />
     </v-list>
   </v-navigation-drawer>
 </template>
 <script>
-  
-  import NavigationItem from "@/components/navigation/NavigationItem.vue";
-  
-  export default {
-    name: 'Navigation', 
+import NavigationItem from '@/components/navigation/NavigationItem.vue';
 
-    components: {
-      NavigationItem
-    },
+export default {
+  name: 'Navigation',
 
-    data: () => ({
-      drawer: true,
-      items: [
-        { title: 'Dashboard', icon: 'mdi-view-dashboard', action: '/' },
-        { title: 'Products', icon: 'mdi-cart', action: '/products' },
-        { title: 'Categories', icon: 'mdi-store', action: '/categories' },
-        { title: 'Share', icon: 'mdi-share', action: '/' },
-        { title: 'Logout', icon: 'mdi-exit-to-app', action: '/logout' },
-      ],
-      mini: true,
-    }),
-  };
+  components: {
+    NavigationItem,
+  },
+
+  data: () => ({
+    drawer: true,
+    items: [
+      { title: 'Dashboard', icon: 'mdi-view-dashboard', action: '/' },
+      { title: 'Products', icon: 'mdi-cart', action: '/products' },
+      { title: 'Categories', icon: 'mdi-store', action: '/categories' },
+      { title: 'Share', icon: 'mdi-share', action: '/' },
+      { title: 'Logout', icon: 'mdi-exit-to-app', action: '/logout' },
+    ],
+    mini: true,
+  }),
+};
 </script>
