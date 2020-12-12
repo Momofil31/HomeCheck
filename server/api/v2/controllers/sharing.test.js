976068e51dcc6cb39d20d995f2330deeab0d6531
Marcolech <lechthalermarco@gmail.com>
2020-12-12 14:06:45 +0100

    :white_check_mark: Add sharing tests

---
+++ server/api/v2/controllers/sharing.test.js
@@ -0,0 +1,93 @@
+const supertest = require('supertest');
+const mongoose = require('mongoose');
+const util = require('./testUtil');
+const app = require('../../../app');
+const Sharing = require('../../../models/Sharing');
+
+const server = supertest(app);
+
+const basePath = '/v2/sharing';
+let testUser;
+
+describe('Test sharing controller', () => {
+  beforeAll(async () => {
+    testUser = await util.getTestUserAuthToken(server);
+  });
+
+  beforeEach(async () => {
+    await util.clearSharingTable();
+  });
+
+  afterAll(async (done) => {
+    await mongoose.disconnect();
+    done();
+  });
+
+  test('GET sharing token', async () => {
+    const sharing = {
+      _id: mongoose.Types.ObjectId(),
+      user: testUser.user.id,
+    };
+
+    await Sharing.create(sharing);
+
+    const response = await server
+      .get(`${basePath}/token`)
+      .set('Authorization', `Bearer ${testUser.token}`)
+      .send();
+
+    expect(response.status).toBe(200);
+    expect(response.body.data.sharing.token).toBe(sharing._id.toString());
+  });
+
+  test('GET sharing token 404', async () => {
+    const response = await server
+      .get(`${basePath}/token`)
+      .set('Authorization', `Bearer ${testUser.token}`)
+      .send();
+
+    expect(response.status).toBe(404);
+  });
+
+  test('POST generate sharing token', async () => {
+    const response = await server
+      .post(`${basePath}/token`)
+      .set('Authorization', `Bearer ${testUser.token}`)
+      .send();
+
+    expect(response.status).toBe(201);
+
+    expect(response.body).toMatchObject({
+      data: {
+        message: expect.stringMatching(/.*/),
+        sharing: {
+          token: expect.stringMatching(/.*/),
+          request: expect.anything(),
+        },
+      },
+    });
+
+    const sharing = await Sharing.findOne({ user: testUser.user.id })
+      .exec()
+      .then((category) => category)
+      .catch(() => null);
+
+    expect(response.body.data.sharing.token).toBe(sharing._id.toString());
+  });
+
+  test('POST sharing token already present', async () => {
+    const sharing = {
+      _id: mongoose.Types.ObjectId(),
+      user: testUser.user.id,
+    };
+
+    await Sharing.create(sharing);
+
+    const response = await server
+      .post(`${basePath}/token`)
+      .set('Authorization', `Bearer ${testUser.token}`)
+      .send();
+
+    expect(response.status).toBe(409);
+  });
+});
