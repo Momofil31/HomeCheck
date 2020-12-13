3a357c9bdb7de31574596fa05fcfff5c96337fb5
Momofil31 <filippomomesso@gmail.com>
2020-12-13 23:56:51 +0100

    :sparkles: Switch to gmail email

--- server/api/v2/controllers/mailUtil.js
+++ server/api/v2/controllers/mailUtil.js
@@ -1,21 +1,19 @@
 const nodemailer = require('nodemailer');
 
+const user = process.env.MAIL_ADDRESS;
+const pass = process.env.MAIL_PW;
+
+const transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true, // use SSL
+  auth: {
+    user,
+    pass,
+  },
+});
+
 exports.resetPasswordMail = async (email, password) => {
-  // Generate test SMTP service account from ethereal.email
-  // Only needed if you don't have a real mail account for testing
-  const testAccount = await nodemailer.createTestAccount();
-
-  // create reusable transporter object using the default SMTP transport
-  const transporter = nodemailer.createTransport({
-    host: 'smtp.ethereal.email',
-    port: 587,
-    secure: false, // true for 465, false for other ports
-    auth: {
-      user: testAccount.user, // generated ethereal user
-      pass: testAccount.pass, // generated ethereal password
-    },
-  });
-
   const htmlMail = `<p>Here is your new password.</p>
                       <h4>${password}</h4> 
                     `;
@@ -23,7 +21,7 @@ exports.resetPasswordMail = async (email, password) => {
 
   // send mail with defined transport object
   const info = await transporter.sendMail({
-    from: '"Homecheck" <no-reply@homecheck.com>', // sender address
+    from: `"Homecheck" ${user}`, // sender address
     to: `${email}`, // list of receivers
     subject: 'Reset Password', // Subject line
     text: textMail, // plain text body
@@ -38,21 +36,6 @@ exports.resetPasswordMail = async (email, password) => {
 };
 
 exports.confirmationEmail = async (email, name, link, token) => {
-  // Generate test SMTP service account from ethereal.email
-  // Only needed if you don't have a real mail account for testing
-  const testAccount = await nodemailer.createTestAccount();
-
-  // create reusable transporter object using the default SMTP transport
-  const transporter = nodemailer.createTransport({
-    host: 'smtp.ethereal.email',
-    port: 587,
-    secure: false, // true for 465, false for other ports
-    auth: {
-      user: testAccount.user, // generated ethereal user
-      pass: testAccount.pass, // generated ethereal password
-    },
-  });
-
   const htmlMail = `<p>Hello ${name}, <br/>to confirm your account and start using Homecheck go to this <a href="${link}">link</a></p>
                     <p>If you want to use the API instead here is the token: ${token}</p>`;
   const textMail = `Hello ${name}, to confirm your account and start using Homecheck go to this link: ${link}.
@@ -60,7 +43,7 @@ exports.confirmationEmail = async (email, name, link, token) => {
 
   // send mail with defined transport object
   const info = await transporter.sendMail({
-    from: '"Homecheck" <no-reply@homecheck.com>', // sender address
+    from: `"Homecheck" ${user}`, // sender address
     to: `${email}`, // list of receivers
     subject: 'Email Confirmation', // Subject line
     text: textMail, // plain text body
