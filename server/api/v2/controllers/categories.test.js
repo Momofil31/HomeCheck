b8b1505d1cd8cfb7787726d08a4eac3dacc76e57
Marcolech <lechthalermarco@gmail.com>
2020-12-13 15:49:13 +0100

    :white_check_mark: Add categories update and delete tests

--- server/api/v2/controllers/categories.test.js
+++ server/api/v2/controllers/categories.test.js
@@ -62,7 +62,7 @@ describe('Test category controller', () => {
       default: false,
     });
 
-    await category.save();
+    await Category.create(category);
 
     const response = await server
       .get(`${basePath}/${categoryId}`)
@@ -89,7 +89,7 @@ describe('Test category controller', () => {
       default: false,
     });
 
-    await category.save();
+    await Category.create(category);
 
     const response = await server
       .get(`${basePath}/${mongoose.Types.ObjectId()}`)
@@ -110,7 +110,7 @@ describe('Test category controller', () => {
       default: false,
     });
 
-    await category.save();
+    await Category.create(category);
 
     const response = await server
       .get(`${basePath}/${categoryId}test`)
@@ -142,5 +142,89 @@ describe('Test category controller', () => {
         },
       },
     });
+  });
+
+  test('POST create a category already present', async () => {
+    const category = new Category({
+      _id: mongoose.Types.ObjectId(),
+      name: 'Test1',
+      icon: 'Test1.png',
+      user: testUser.user.id,
+      default: false,
+    });
+
+    await Category.create(category);
+
+    const response = await server
+      .post(`${basePath}`)
+      .set('Authorization', `Bearer ${testUser.token}`)
+      .send({
+        name: 'Test1',
+        icon: 'Test1.png',
+        default: false,
+      });
+
+    expect(response.status).toBe(409);
+  });
+
+  test('PUT update a category', async () => {
+    const categoryId = mongoose.Types.ObjectId();
+    const category = new Category({
+      _id: categoryId,
+      name: 'Test1',
+      icon: 'Test1.png',
+      user: testUser.user.id,
+      default: false,
+    });
+
+    await Category.create(category);
+
+    const response = await server
+      .put(`${basePath}/${categoryId}`)
+      .set('Authorization', `Bearer ${testUser.token}`)
+      .send({
+        name: 'Test2',
+        icon: 'Test2.png',
+        default: false,
+      });
+
+    expect(response.status).toBe(200);
+
+    const updatedCategory = await Category.findById(categoryId)
+      .exec()
+      .then((c) => c)
+      .catch(() => null);
+
+    expect(updatedCategory._id.toString()).toBe(categoryId.toString());
+    expect(updatedCategory.name).toBe('Test2');
+    expect(updatedCategory.icon).toBe('Test2.png');
+    expect(updatedCategory.user.toString()).toBe(testUser.user.id);
+    expect(updatedCategory.default).toBe(false);
+  });
+
+  test('DELETE a category', async () => {
+    const categoryId = mongoose.Types.ObjectId();
+    const category = new Category({
+      _id: categoryId,
+      name: 'Test1',
+      icon: 'Test1.png',
+      user: testUser.user.id,
+      default: false,
+    });
+
+    await Category.create(category);
+
+    const response = await server
+      .delete(`${basePath}/${categoryId}`)
+      .set('Authorization', `Bearer ${testUser.token}`)
+      .send();
+
+    expect(response.status).toBe(200);
+
+    const deletedCategory = await Category.findById(categoryId)
+      .exec()
+      .then((c) => c)
+      .catch(() => null);
+    expect(deletedCategory).toBe(null);
   });
 });
