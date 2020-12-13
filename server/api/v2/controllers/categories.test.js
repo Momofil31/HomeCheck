09b95dcb10aa38ce25c1463e10f5c37e068dda12
Momofil31 <filippomomesso@gmail.com>
2020-12-13 23:06:20 +0100

    :white_check_mark: Add more categories tests

--- server/api/v2/controllers/categories.test.js
+++ server/api/v2/controllers/categories.test.js
@@ -120,6 +120,64 @@ describe('Test category controller', () => {
     expect(response.status).toBe(400);
   });
 
+  test('GET one category 403 unathorized user', async () => {
+    const categoryId = mongoose.Types.ObjectId();
+
+    const category = new Category({
+      _id: categoryId,
+      name: 'Test1',
+      icon: 'Test1.png',
+      user: testUser.user.id,
+      default: false,
+    });
+
+    await Category.create(category);
+
+    const differentTestUser = await util.getTestUserAuthToken(server);
+
+    const response = await server
+      .get(`${basePath}/${categoryId}`)
+      .set('Authorization', `Bearer ${differentTestUser.token}`)
+      .send();
+
+    expect(response.status).toBe(403);
+  });
+
+  test('POST create a category should succeed because category already exists but belongs to a different user', async () => {
+    const newUser = await util.getTestUserAuthToken(server);
+
+    const category = new Category({
+      _id: mongoose.Types.ObjectId(),
+      name: 'Category',
+      icon: 'category.png',
+      user: newUser.user.id,
+      default: false,
+    });
+
+    await Category.create(category);
+    const response = await server
+      .post(`${basePath}`)
+      .set('Authorization', `Bearer ${testUser.token}`)
+      .send({
+        name: 'Category',
+        icon: 'category.png',
+        default: false,
+      });
+
+    expect(response.status).toBe(201);
+    expect(response.body).toMatchObject({
+      data: {
+        message: expect.stringMatching(/.*/),
+        category: {
+          id: expect.stringMatching(/.*/),
+          name: 'Category',
+          icon: 'category.png',
+          default: false,
+        },
+      },
+    });
+  });
+
   test('POST create a category ', async () => {
     const response = await server
       .post(`${basePath}`)
@@ -167,6 +225,68 @@ describe('Test category controller', () => {
     expect(response.status).toBe(409);
   });
 
+  test('PUT update a category should fail because name and icon are not filled in', async () => {
+    const categoryId = mongoose.Types.ObjectId();
+    const category = new Category({
+      _id: categoryId,
+      name: 'Test1',
+      icon: 'Test1.png',
+      user: testUser.user.id,
+      default: false,
+    });
+
+    await Category.create(category);
+
+    const response = await server
+      .put(`${basePath}/${categoryId.toString()}`)
+      .set('Authorization', `Bearer ${testUser.token}`)
+      .send({
+        name: '',
+        icon: '',
+        default: false,
+      });
+
+    const desiredResponse = {
+      error: {
+        message: 'Update failed.',
+        description: 'Name or icon are not filled in with the corresponding data.',
+      },
+    };
+    expect(response.status).toBe(400);
+    expect(response.body).toMatchObject(desiredResponse);
+  });
+
+  test("PUT update category should fail because category doesn't exists", async () => {
+    const categoryId = mongoose.Types.ObjectId();
+    const category = new Category({
+      _id: categoryId,
+      name: 'Test1',
+      icon: 'Test1.png',
+      user: testUser.user.id,
+      default: false,
+    });
+
+    await Category.create(category);
+
+    const notExistingCategoryId = mongoose.Types.ObjectId();
+    const response = await server
+      .put(`${basePath}/${notExistingCategoryId.toString()}`)
+      .set('Authorization', `Bearer ${testUser.token}`)
+      .send({
+        name: 'Test1',
+        icon: 'Test1.png',
+        default: false,
+      });
+    const desiredResponse = {
+      error: {
+        message: 'Update failed',
+        description: 'Category not found',
+      },
+    };
+    expect(response.status).toBe(404);
+    expect(response.body).toMatchObject(desiredResponse);
+  });
+
   test('PUT update a category', async () => {
     const categoryId = mongoose.Types.ObjectId();
     const category = new Category({
@@ -202,7 +322,67 @@ describe('Test category controller', () => {
     expect(updatedCategory.default).toBe(false);
   });
 
-  test('DELETE a category', async () => {
+  test("DELETE category should fail because category doesn't exist", async () => {
+    const categoryId = mongoose.Types.ObjectId();
+    const category = new Category({
+      _id: categoryId,
+      name: 'Test1',
+      icon: 'Test1.png',
+      user: testUser.user.id,
+      default: false,
+    });
+
+    await Category.create(category);
+
+    const notExistingCategoryId = mongoose.Types.ObjectId();
+
+    const response = await server
+      .delete(`${basePath}/${notExistingCategoryId}`)
+      .set('Authorization', `Bearer ${testUser.token}`)
+      .send();
+
+    const desiredResponse = {
+      error: {
+        message: 'Deletion failed',
+        description: 'Category not found',
+      },
+    };
+
+    expect(response.status).toBe(404);
+    expect(response.body).toMatchObject(desiredResponse);
+  });
+
+  test('DELETE category should fail because user is not authorized to delete', async () => {
+    const categoryId = mongoose.Types.ObjectId();
+    const category = new Category({
+      _id: categoryId,
+      name: 'Test1',
+      icon: 'Test1.png',
+      user: testUser.user.id,
+      default: false,
+    });
+
+    await Category.create(category);
+
+    const newUser = await util.getTestUserAuthToken(server);
+
+    const response = await server
+      .delete(`${basePath}/${categoryId}`)
+      .set('Authorization', `Bearer ${newUser.token}`)
+      .send();
+
+    const desiredResponse = {
+      error: {
+        message: 'Deletion failed.',
+        description: `User not authorized to delete category ${category.name}.`,
+      },
+    };
+
+    expect(response.status).toBe(403);
+    expect(response.body).toMatchObject(desiredResponse);
+  });
+
+  test('DELETE category should succeed', async () => {
     const categoryId = mongoose.Types.ObjectId();
     const category = new Category({
       _id: categoryId,
