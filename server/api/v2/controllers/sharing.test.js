1f3b14231146e3c4149a3037f991bf9e7ff8b91e
Marcolech <lechthalermarco@gmail.com>
2020-12-14 01:03:05 +0100

    :white_check_mark: Add get sharing products test

--- server/api/v2/controllers/sharing.test.js
+++ server/api/v2/controllers/sharing.test.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const util = require('./testUtil');
 const app = require('../../../app');
 const Sharing = require('../../../models/Sharing');
+const Products = require('../../../models/Product');
 
 const server = supertest(app);
 
@@ -12,6 +13,12 @@ let testUser;
 describe('Test sharing controller', () => {
   beforeAll(async () => {
     testUser = await util.getTestUserAuthToken(server);
+    await util.clearGroupTable();
+    await util.clearCategoryTable();
+    await util.clearProductTable();
+    const category = await util.createTestCategory(testUser.user.id);
+    const group = await util.createTestGroup();
+    await util.createTestProducts(category, group, testUser.user.id);
   });
 
   beforeEach(async () => {
@@ -131,5 +138,70 @@ describe('Test sharing controller', () => {
 
     // no token in db
     expect(sharingToken).toBe(null);
+  });
+
+  test('GET GetList shared products should succeed', async () => {
+    const sharing = {
+      _id: mongoose.Types.ObjectId(),
+      user: testUser.user.id,
+    };
+    await Sharing.create(sharing);
+
+    const response = await server
+      .get(`${basePath}/${sharing._id}/products`)
+      .send();
+
+    expect(response.status).toBe(200);
+    expect(response.body.data.products.length).toBe(3);
+  });
+
+  test('GET GetList shared products should failed when token is deleted', async () => {
+    const sharing = {
+      _id: mongoose.Types.ObjectId(),
+      user: testUser.user.id,
+    };
+    await Sharing.create(sharing);
+
+    // product shared correctly
+    const response = await server
+      .get(`${basePath}/${sharing._id}/products`)
+      .send();
+
+    expect(response.status).toBe(200);
+    expect(response.body.data.products.length).toBe(3);
+
+    // delete token
+    const response2 = await server
+      .delete(`${basePath}/token`)
+      .set('Authorization', `Bearer ${testUser.token}`)
+      .send();
+
+    expect(response2.status).toBe(200);
+
+    // error when access to products
+    const response3 = await server
+      .get(`${basePath}/${sharing._id}/products`)
+      .send();
+
+    expect(response3.status).toBe(403);
+  });
+
+  test('GET GetOne shared product should succeed', async () => {
+    const sharing = {
+      _id: mongoose.Types.ObjectId(),
+      user: testUser.user.id,
+    };
+    await Sharing.create(sharing);
+
+    const productToFind = await Products.find()
+      .exec()
+      .then((products) => products[0]);
+
+    const response = await server
+      .get(`${basePath}/${sharing._id}/products/${productToFind._id}`)
+      .send();
+
+    expect(response.status).toBe(200);
+    expect(response.body.data.product.id).toBe(productToFind._id.toString());
   });
 });
